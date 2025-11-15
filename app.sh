f4d8a8d1014c80ddf941ed3eb849deac27a5de07
hikalputra12 <haikal.putra1210@gmail.com>
2025-11-16 02:01:15 +0800

    menambahkan fungsi untuk pengecekan informasi sistem

--- app.sh
+++ app.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo -e "=== Selamat datang di program pengecekan Generator Laporan Kesehatan Sistem device komputer ===\n"
+
+informasi_sistem(){ 
+    sistem=$(uname -a)
+    echo "$sistem"
+}
