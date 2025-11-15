7c25121a88e4a3ed2c82a89d1e66e1baead7aab6
hikalputra12 <haikal.putra1210@gmail.com>
2025-11-16 02:16:38 +0800

    menamambahkan fungsi untuk pengecekan penggunaan memori

--- app.sh
+++ app.sh
@@ -13,3 +13,11 @@ penggunaan_CPU(){
     cpu_use=$(top -bn1)
     echo "$cpu_use"  #perintah untuk mengecek penggunaan CPU
 }
+
+#fungsi untuk pengecekan penggunaan_memori
+penggunaan_memori(){
+    memori=$(free -h)
+    echo "$memori" #perintah untuk mengecek penggunaan_memori
+}
+
+
