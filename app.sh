4a0349f700fe87f857327502b95905534821270b
hikalputra12 <haikal.putra1210@gmail.com>
2025-11-16 02:23:45 +0800

    menambahkan fungsi main sebagai fungsi utama menjalankan seluruh program

--- app.sh
+++ app.sh
@@ -31,3 +31,41 @@ mengecek_jaringan(){
     jaringan=$(ss -tuln) #perintah untuk mengecek jaringan
     echo "$jaringan"
 }
+
+#fungsi main untuk pemilihan menjalankan seluruh program
+main() {
+    while true; do #melakukan pengulangan program
+        echo "===pilihlah opsi berikut untuk mengecek kesehatan sistem anda ===="
+        echo "(1). informasi sistem"
+        echo "(2). penggunaan_CPU"
+        echo "(3). penggunaan_memori"
+        echo "(4). penggunaan_ruang_disk"
+        echo "(5). mengecek_jaringan"
+        echo "(6). keluar dari program"
+        read -p "masukkan pilihan anda(1-6): " pilihan
+        #kondisi berjalan sesuai perintah yang di input
+        case "$pilihan" in
+            "1")
+                informasi_sistem
+                ;;
+            "2")
+                penggunaan_CPU
+                ;;
+            "3")
+                penggunaan_memori
+                ;;
+            "4")
+                penggunaan_ruang_disk
+                ;;
+            "5") 
+                mengecek_jaringan
+                ;;
+            "6")
+                break;;
+            *) 
+                echo "Pilihan anda tidak valid. silakan pilih angka 1-6"
+                ;;
+        esac 
+    done
+}
+main
